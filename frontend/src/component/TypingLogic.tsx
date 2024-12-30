922c6fb0c35a57b65122192a558ab56dce2ed685
Ameer jafar <ameerjafar123@gmail.com>
2024-12-30 12:51:02 +0530

    added

--- frontend/src/component/TypingLogic.tsx
+++ frontend/src/component/TypingLogic.tsx
@@ -1,55 +1,64 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState, createRef } from "react";
 import CursorBlinker from "../ui/CursorBlinker";
 import { useRecoilState, useSetRecoilState } from "recoil";
-import { inCorrectedAtom, textAtom } from "../store/textAtom";
+import { textAtom } from "../store/textAtom";
 import { paragraphActive, paragraphFocus } from "../store/paragraph";
 import CounterComponent from "./CounterComponent";
 import { randomWord } from "./wordList";
+
+
 const TypingLogic = () => {
   const [text, setText] = useRecoilState(textAtom);
+  const [wordCharacter, setWordCharacter] = useState<string[]>();
   const [words, setWords] = useState<string[]>(randomWord);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [focus, setFocus] = useRecoilState(paragraphFocus);
   const setIsActive = useSetRecoilState(paragraphActive);
-  const setInCorrectCharacter = useSetRecoilState(inCorrectedAtom);
-  const [ correct, setCorrect ] = useState<boolean[]>([]);
-  const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
+  const [ currentWordIndex, setCurrentWordIndex ] = useState(0);
+  const emptySpans = ()=> {
+    return Array(words.length)
+    .fill(0)
+    .map((i) => createRef<HTMLSpanElement>());
+  }
+  const [ wordSpanRef, setWordSpanRef ] = useState(emptySpans());
 
-  useEffect(() => {
-    const fetchParagraph = async () => {
-      console.log(import.meta.env.VITE_API_PATH);
-      const response = await axios.get(`${import.meta.env.VITE_API_PATH}`);
-      setParagraph(response.data.paragraph);
-      console.log(response.data.paragraph);
-    };
-    fetchParagraph();
-  }, []);
   const handleKeyDown = (event) => {
     const { key } = event;
     console.log(key);
     setIsActive(true);
     if (key === " ") {
       setText([...text, " "]);
+      setCurrentWordIndex(prev => prev + 1);
     } else if (key === "Backspace") {
+      if(text[currentIndex] === " ") {setCurrentWordIndex(prev => prev - 1)}
       setText(text.slice(0, text.length - 1));
     } else if (/^[a-zA-Z.,]$/.test(key)) {
       setText([...text, key]);
     }
+    
     setCurrentIndex(text.length);
-    if (currentIndex + 1 === words[currentWordIndex].length) {
-      setCurrentWordIndex((prev) => prev + 1);
-    }
   };
+
   useEffect(() => {
-    for(let i = 0; i < text.length; i++) {
-      if(word[currentWordIndex] === text[i]) {
-        
-      }
+    const wordCharacter = words
+         .map(word => {
+           return [...word.split(''), ' ']; 
+         })
+         .flat();
+    setWordCharacter(wordCharacter);
+  }, []);
+  useEffect(() => {
+    console.log(wordSpanRef);
+    console.log(currentWordIndex);
+    console.log(wordSpanRef[currentWordIndex].current?.offsetHeight, wordSpanRef[currentWordIndex + 1].current?.offsetLeft);
+    if(wordSpanRef[currentWordIndex + 1].current!.offsetLeft < wordSpanRef[currentWordIndex].current!.offsetLeft) {
+      wordSpanRef[currentWordIndex].current!.scrollIntoView({
+        behavior: "smooth"
+      })
     }
-  }, [ text, words ])
+  },[currentWordIndex])
   return (
-    <div>
+    <div className="">
       <CounterComponent />
       <div
         tabIndex={0}
@@ -57,7 +66,7 @@ const TypingLogic = () => {
         onFocus={() => setFocus(true)}
       >
         {!focus && (
-          <div className="absolute z-0 flex justify-center w-full h-full items-center text-white">
+          <div className="absolute z-0 flex mb-3 justify-center w-full h-full items-center text-white">
             click here to focus on the content
           </div>
         )}
@@ -66,13 +75,31 @@ const TypingLogic = () => {
             <CursorBlinker />
           </div>
         )}
+        <div className="absolute z-0 leading-loose">
+          {text.map((character, ind) => {
+            const inCorrect = wordCharacter![ind] !== character
+            const i = ind === currentIndex;
+            console.log(wordCharacter);
+            return (
+              <span key={ind}>
+                {inCorrect ? <span className={`${wordCharacter![ind] === " "? "bg-red-500": "text-red-500"}`}>{ wordCharacter[ind] }</span>: 
+                <span className="text-white">{ character }</span>
+                }
+                {i && <CursorBlinker />}
+              </span>
+            )
+          })}
+        </div>
         <div
+
           className={`leading-loose ${
             focus ? "blur-none" : "blur-sm"
           } overflow-hidden h-52`}
         >
           {words.map((word, ind) => {
-            return <span key={ind}>{word + " "}</span>;
+            return (
+              <span className = 'inline-block mr-5' ref = { wordSpanRef[ind] }  key={ind}> {word}</span>
+            )
           })}
         </div>
       </div>
