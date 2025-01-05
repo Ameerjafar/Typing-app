81623874a07f54b7270ac2d54e7aa0f023fbd9e3
Ameer jafar <ameerjafar123@gmail.com>
2025-01-05 21:00:21 +0530

    added the command

--- frontend/src/component/Result.tsx
+++ frontend/src/component/Result.tsx
@@ -1,25 +1,19 @@
 
 import WpmComponent from "./WpmComponent";
-import { updateTestCompleted, updateTestInformation } from "./UpdataUserData";
-import { useEffect, useRef, useState } from "react";
-import { useRecoilValue } from "recoil";
-import { timeAtom } from "../store/TimeAtom";
+import { updateTestCompleted } from "./UpdataUserData";
+import { useEffect, useRef } from "react";
 const Result = () => {
-  const [ wpm, setWpm ] = useState<string>("0");
-  const [ accuracy, setAccuracy ] = useState<string>("0"); 
-  const time = useRecoilValue(timeAtom);
   const hasEffectRun = useRef(false);
   useEffect(() => {
     if(hasEffectRun.current === true) return;
     updateTestCompleted();
-    updateTestInformation(wpm, time, accuracy);
     hasEffectRun.current = true;
-  }, [wpm])
+  }, [])
   return (
     <div className = 'bg-[#121212] w-full h-screen flex justify-center items-center'>
-      <WpmComponent wpm = { wpm } setWpm = { setWpm } accuracy = { accuracy } setAccuracy = { setAccuracy }  />
+      <WpmComponent />
     </div>
   )
 }
-
+// here i have to make the api call for the type information.
 export default Result;
